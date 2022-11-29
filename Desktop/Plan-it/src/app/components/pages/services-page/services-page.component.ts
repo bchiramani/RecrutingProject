6df148d6085388bb5b3aec6b310bc479e56f1d6f
bchiramani <amanibchir00@gmail.com>
2022-11-29 23:10:39 +0100

    Model : SProvider , dynamique display of SProviders in the home page and in the services page . working on filters and search

---
+++ Desktop/Plan-it/src/app/components/pages/services-page/services-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SProvider } from 'src/app/models/SProvider';
+import { User } from 'src/app/models/User';
+import { SproviderService } from 'src/app/services/sprovider.service';
+
+@Component({
+  selector: 'app-services-page',
+  templateUrl: './services-page.component.html',
+  styleUrls: ['./services-page.component.css']
+})
+export class ServicesPageComponent implements OnInit {
+  sproviders:Array<SProvider>
+  category: string=""
+  constructor(private sproviderservice: SproviderService) { }
+
+  ngOnInit() {
+    this.sproviders=this.sproviderservice.getAll()
+    console.log(this.sproviders)
+  }
+  onSelect(){
+    if (this.category ==""){
+      this.sproviders = this.sproviderservice.getAll()
+    }else{
+      this.sproviders = this.sproviderservice.getByCategory(this.category)
+    }
+    console.log(this.sproviders)
+  }
+
+}
